25ec511331b501807808af32490a66cd984e5ba8
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-11 14:25:47 +0300

    Created home component and add logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RepoComponent } from './repo/repo.component';
 import { UserComponent } from "./user/user.component";
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path:'home',component:HomeComponent},
   {path:'users', component: UserComponent},
   {path:'repos', component:RepoComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
-import { ApiRequestService } from './api-http/api-request.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RepoComponent } from './repo/repo.component';
 import { UserComponent } from './user/user.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RepoComponent,
     UserComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
